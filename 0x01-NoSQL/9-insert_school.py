4204836b791455653ca4837466f41b5eeec18842
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-26 23:08:20 +0000

    Insert a new doument using python

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Insert a new document into a collection
+"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """Insert a new document in a collection based on kwargs."""
+    return mongo_collection.insert_one(kwargs).inserted_id
