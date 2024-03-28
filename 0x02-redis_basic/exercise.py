98300ec6181683378ddebda655b88c9c5472d0d6
wamae-ndiritu <wamaejoseph392@gmail.com>
2024-03-28 00:39:16 +0000

    Read from redis and convert to original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Writing strings to Redis
 """
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Union
 
 
 class Cache:
@@ -25,3 +25,51 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Callable = None) -> Union
+    [str, bytes, int, float, None]:
+        """
+        Retrieve data from the Redis cache.
+
+        Args:
+            key (str): The key under which the data is stored.
+            fn (Callable, optional): A function to convert the
+            retrieved data to the desired format.
+
+        Returns:
+            Union[str, bytes, int, float, None]: The retrieved data,
+            optionally converted using the provided function.
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieve data from the Redis cache
+        and convert it to a string.
+
+        Args:
+            key (str): The key under which the data is stored.
+
+        Returns:
+            Union[str, None]: The retrieved data as a string,
+            or None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieve data from the Redis cache and convert it to an integer.
+
+        Args:
+            key (str): The key under which the data is stored.
+
+        Returns:
+            Union[int, None]: The retrieved data as an integer,
+            or None if the key does not exist.
+        """
+        return self.get(key, fn=lambda d: int(d))
